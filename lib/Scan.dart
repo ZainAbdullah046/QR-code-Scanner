bae57a894421048f56863c5ab50597803ac45268
ZainAbdullah046 <zainabdullah222046@gmail.com>
2024-08-10 12:37:24 +0500

    QR_CODE_SCANNER

--- lib/Scan.dart
+++ lib/Scan.dart
@@ -1,4 +1,7 @@
+import 'dart:typed_data';
+
 import 'package:flutter/material.dart';
+import 'package:mobile_scanner/mobile_scanner.dart';
 import 'package:qr_code_scanner_app/Generate.dart';
 
 class Scan extends StatefulWidget {
@@ -29,6 +32,30 @@ class _MyWidgetState extends State<Scan> {
               },
               icon: const Icon(Icons.qr_code))
         ],
+      ),
+      body: MobileScanner(
+        controller: MobileScannerController(
+          detectionSpeed: DetectionSpeed.noDuplicates,
+          returnImage: true,
+        ),
+        onDetect: (capture) {
+          final List<Barcode> barcodes = capture.barcodes;
+          final Uint8List? image = capture.image;
+
+          for (final barcode in barcodes) {
+            print("Found ${barcode.rawValue}");
+          }
+          if (image != null) {
+            showDialog(
+                context: context,
+                builder: (context) {
+                  return AlertDialog(
+                    title: Text(barcodes.first.rawValue ?? ""),
+                    content: Image(image: MemoryImage(image)),
+                  );
+                });
+          }
+        },
       ),
     );
   }
