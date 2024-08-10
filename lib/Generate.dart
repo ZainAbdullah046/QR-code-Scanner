d593b3fa6182e1a6c1f8915dbeb320971e59ff4f
ZainAbdullah046 <zainabdullah222046@gmail.com>
2024-08-10 12:46:09 +0500

    QR_CODE_SCANNER

--- lib/Generate.dart
+++ lib/Generate.dart
@@ -9,27 +9,43 @@ class Generate extends StatefulWidget {
 }
 
 class _GenerateState extends State<Generate> {
+  String? qrdata;
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        backgroundColor: Color.fromARGB(255, 52, 113, 162),
-        title: const Text(
-          "GENERATE QR CODE",
-          style: TextStyle(
-              color: Color.fromARGB(179, 202, 197, 197),
-              fontWeight: FontWeight.bold,
-              fontSize: 15),
+        appBar: AppBar(
+          backgroundColor: Color.fromARGB(255, 52, 113, 162),
+          title: const Text(
+            "GENERATE QR CODE",
+            style: TextStyle(
+                color: Color.fromARGB(179, 202, 197, 197),
+                fontWeight: FontWeight.bold,
+                fontSize: 15),
+          ),
+          actions: [
+            IconButton(
+                onPressed: () {
+                  Navigator.pushReplacement(
+                      context, MaterialPageRoute(builder: (context) => Scan()));
+                },
+                icon: const Icon(Icons.qr_code_scanner))
+          ],
         ),
-        actions: [
-          IconButton(
-              onPressed: () {
-                Navigator.pushReplacement(
-                    context, MaterialPageRoute(builder: (context) => Scan()));
-              },
-              icon: const Icon(Icons.qr_code_scanner))
-        ],
-      ),
-    );
+        body: Padding(
+          padding: const EdgeInsets.all(10),
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+            crossAxisAlignment: CrossAxisAlignment.center,
+            children: [
+              TextField(
+                onSubmitted: (value) {
+                  setState(() {
+                    qrdata = value;
+                  });
+                },
+              ),
+            ],
+          ),
+        ));
   }
 }
